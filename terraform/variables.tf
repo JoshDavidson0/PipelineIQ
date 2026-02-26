ae720fa4561144e5cb86b286aa34cc9e1ebfe218
JoshDavidson0 <joshuald226@gmail.com>
2026-02-26 13:16:04 -0500

    Variables and providers defined in variables.tf and main.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+# variable.tf defines all input variables
+
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "project_name" {
+    default = "pipelineiq"
+}
+
+variable "db_username" {
+    description = "Postgres master username"
+    type = string
+    sensitive = true
+}
+
+variable "db_password" {
+    description = "Postgres master password"
+    type = string
+    sensitive = true
+}
