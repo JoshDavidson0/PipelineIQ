2950376e4514dcec4fccc243a86045ea9925d5c7
JoshDavidson0 <joshuald226@gmail.com>
2026-02-27 11:10:57 -0500

    outputs.tf created

---
+++ terraform/outputs.tf
@@ -0,0 +1,17 @@
+# outputs.tf defines the values terraform will print to the terminal after terraform apply completes
+
+
+output "s3_bucket_name" {
+    description = "The name of the uploads bucket, including the random suffix"
+    value = aws_s3_bucket.uploads.bucket
+}
+
+output "rds_endpoint" {
+    description = "The connection endpoint for the Postgres database"
+    value = aws_db_instance.postgres.endpoint
+}
+
+output "rds_db_name" {
+    description = "The name of the Postgres database inside the RDS instance"
+    value = aws_db_instance.postgres.db_name
+}
