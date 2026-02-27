566da2d934c522092485634d6de8e13ea6be2e88
JoshDavidson0 <joshuald226@gmail.com>
2026-02-27 09:27:12 -0500

    s3.tf created

---
+++ terraform/s3.tf
@@ -0,0 +1,23 @@
+# s3.tf defines the s3 bucket where the user will upload images to.
+# versioning will preserve duplicate files instead of overwriting the first.
+
+
+resource "aws_s3_bucket" "uploads" {
+    bucket = "${var.project_name}--uploads-${random_id.suffix.hex}"
+
+    tags = {
+        Project = var.project_name
+    }
+}
+
+resource "aws_s3_bucket_versioning" "uploads" {
+    bucket = aws_s3_bucket.uploads.id
+
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+resource "random_id" "suffix" {
+    byte_length = 4
+}
