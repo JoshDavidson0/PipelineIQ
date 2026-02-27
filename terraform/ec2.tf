f4b685900480bb74e9f9d2ec6c027a3bffb8185f
JoshDavidson0 <joshuald226@gmail.com>
2026-02-27 17:37:28 -0500

    modified ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "ec2" {
 # The goal here is to create the EC2 instance that will run the docker container.
 resource "aws_instance" "app" {
     ami = "ami-0c02fb55956c7d316"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     subnet_id = aws_subnet.public.id
     vpc_security_group_ids = [aws_security_group.ec2.id]
     key_name = var.key_pair_name
