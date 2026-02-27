513f9c69e95a24bd5477fe7c95dd8ddcfa84e985
JoshDavidson0 <joshuald226@gmail.com>
2026-02-27 15:52:57 -0500

    route table and association added to ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,4 +1,5 @@
 # ec2.tf defines the public subnet for the ec2 instance and internet gateway necessary for the VP
+# this file also creates a routing table and connect it to the public subnet. 
 
 resource "aws_subnet" "public" {
     vpc_id = aws_vpc.main.id
@@ -17,4 +18,24 @@ resource "aws_internet_gateway" "main" {
     tags = {
         Name = "${var.project_name}-igw"
     }
+}
+
+# The goal heres is to define the rule that anything going to the public internet should be sent through the internet gateway.
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+        Name = "${var.project_name}-public-rt"
+    }
+}
+
+# The goal here is to connect the route table to the public subnet. 
+resource "aws_route_table_association" "public" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public.id
 }
